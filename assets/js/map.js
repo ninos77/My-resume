8ab6ff81ac6c52a140368052619521a0e9dde462
ninos77 <ninos_zaiya@hotmail.com>
2020-03-29 23:30:47 +0000

    greate a new folder and file js,map.js

---
+++ assets/js/map.js
@@ -0,0 +1,9 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 8,
+        center: {
+            lat: -34.397,
+            lng: 150.644
+        }
+    });
+}
