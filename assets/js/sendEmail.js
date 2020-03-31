8810dcf20226b5e59e52afdd54446670638e07c4
ninos77 <ninos_zaiya@hotmail.com>
2020-03-31 12:14:09 +0000

    email service fix bugs

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,9 +1,9 @@
 function sendMail(contactForm){
     emailjs.send("gmail","my_resume_contact",{
         
-        "from_name":contactForm.name.value,
+        "from_name":contactForm.fullname.value,
         "from_email":contactForm.emailaddress.value,
-        "request_contact":contactForm.projectSummary.value
+        "request_contact":contactForm.projectsummery.value
     })
     .then(function(response){
         console.log("SUCCESS", response);
