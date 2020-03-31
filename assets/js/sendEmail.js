5757845fef83657bd75bf4c6feb14a403c514b6b
ninos77 <ninos_zaiya@hotmail.com>
2020-03-31 21:33:04 +0000

    fix som issue i contact form

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,18 @@
 function sendMail(contactForm){
-    emailjs.send("gmail","my_resume_contact",{
+    emailjs.send("gmail","ninos_contact",{
         
         "from_name":contactForm.fullname.value,
         "from_email":contactForm.emailaddress.value,
         "request_contact":contactForm.projectsummery.value
+        
     })
+    
     .then(function(response){
         console.log("SUCCESS", response);
     },
     function(error){
             console.log("FAILED",error);
     });
-    return false;
+    
+    return  false;
 }
