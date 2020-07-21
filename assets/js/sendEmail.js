9ab35b23244aa76084ff48430b15b164d533b69e
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-07-21 22:45:08 +0000

    Make swedish version for whole the website

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -18,3 +18,24 @@ function sendMail(contactForm){
    
     return false;
 }
+
+function sendMailSe(contactForm){
+    emailjs.send("gmail","ninos_contact",{
+        
+        "from_name":contactForm.fullname.value,
+        "from_email":contactForm.emailaddress.value,
+        "request_contact":contactForm.projectsummery.value
+        
+    })
+    
+    .then(function(response){
+        console.log("SUCCESS", response);
+        window.location.href = "contact-response-msg.html";
+        
+    },
+    function(error){
+            console.log("FAILED",error);
+    });
+   
+    return false;
+}
